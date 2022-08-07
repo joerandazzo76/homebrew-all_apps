8ce3e622a92366834370afc809eb75592b337ef5
Giuseppe Randazzo <joerandazzo76@gmail.com>
2022-08-07 01:17:41 -0400

    added formula

---
+++ homebrew-all_apps.rb
@@ -0,0 +1,10 @@
+class HomebrewAllApps < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/joerandazzo76/homebrew-all_apps/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "7bdbda0c3bee9d0e4db90298fb50d3411b9deae38dd04a3bf2c0b904e22420e2"
+
+  def install
+    bin.install "app_list"
+  end
+end
